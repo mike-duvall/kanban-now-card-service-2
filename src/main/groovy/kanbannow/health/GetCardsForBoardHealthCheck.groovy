8d9a1a55d505f259bb765143a5c880c2d286bd92
Mike DuVall <mike.a.duvall@gmail.com>
2014-06-02 16:28:01 -0500

    Cleaned up health check

--- src/main/groovy/kanbannow/health/GetCardsForBoardHealthCheck.groovy
+++ src/main/groovy/kanbannow/health/GetCardsForBoardHealthCheck.groovy
@@ -51,14 +51,6 @@ public class GetCardsForBoardHealthCheck extends HealthCheck {
                 restClient.get(path: path, requestContentType: 'application/json')
             }
 
-//        def expectedResponse = [
-//                cards: [
-//                        [
-//                                id: 5939,
-//                                text: 'order vitamins'
-//                        ]
-//                    ]
-//                ]
 
             def expectedResponse = [
                     [
@@ -82,54 +74,13 @@ public class GetCardsForBoardHealthCheck extends HealthCheck {
         }
 
 
-
-
-
-//        5939 order vitamins                                                                                                                                                                                                                                                           1         61
-//        2062 Paint outside of patio door                                                                                                                                                                                                                                              1         61
-//        5493 eamil Tara                                                                                                                                                                                                                                                               1         61
-//        5494 reply to other emails                                                                                                                                                                                                                                                    1         61
-//        5841 Buy Radon detector for Christmas                                                                                                                                                                                                                                         1         61
-//        5861 prep for early voting                                                                                                                                                                                                                                                    1         61
-//        5524 email about coffee with girls                                                                                                                                                                                                                                            1         61
-//        5001 Spray paint hardward                                                                                                                                                                                                                                                     1         61
-//        5402 clean hood, install screws                                                                                                                                                                                                                                               1         61
-//        5374 file Teresa's lease                                                                                                                                                                                                                                                      1         61
-//        5419 send email about welcome packages                                                                                                                                                                                                                                        1         61
-
-
     }
-
-//    database:
-//
-//    # the name of your JDBC driver
-//    driverClass: oracle.jdbc.driver.OracleDriver
-//
-//    # the username
-//    user: kanban_now
-//
-//    # the password
-//    password: password
-//
-//    # the JDBC URL
-//    url: jdbc:oracle:thin:@192.168.1.7:1521/ORA11DEV
-//
-//    # the minimum number of connections to keep open
-//    minSize: 1
-//
-//    # the maximum number of connections to keep open
-//    maxSize: 2
-//
-//    # the SQL query to run when validating a connection's liveness
-//    validationQuery: "/* MyService Health Check */ SELECT 1 from dual"
-
 
 
     private void insertTestData() {
 
         DatabaseConfiguration databaseConfiguration = configuration.getDatabaseConfiguration()
 
-//        Sql sql = Sql.newInstance( 'jdbc:jtds:sqlserver://serverName/dbName-CLASS;domain=domainName', 'username', 'password', 'net.sourceforge.jtds.jdbc.Driver' )
         sql = Sql.newInstance(
                 databaseConfiguration.url,
                 databaseConfiguration.user,
@@ -142,7 +93,6 @@ public class GetCardsForBoardHealthCheck extends HealthCheck {
         def location = 1
         sql.execute('insert into card (id, text, board_id, location) values (?,?,?,?)', [9999,text, boardId, location])
 
-        int x = 3
     }
 
     void cleanUpTestData() {
@@ -156,6 +106,5 @@ public class GetCardsForBoardHealthCheck extends HealthCheck {
             return hrex.response
         }
     }
-
 
 }
